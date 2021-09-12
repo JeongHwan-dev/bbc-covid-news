09579d9f4f0c1dd71d473c47b790793bed03df5a
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-09-12 16:13:10 +0900

    Refactor : 스크롤 이벤트 처리 효율 개선

--- js/main.js
+++ js/main.js
@@ -4,10 +4,17 @@
 (() => {
   const stepElems = document.querySelectorAll('.step');
   const graphicElems = document.querySelectorAll('.graphic-item');
-  let currentItem = graphicElems[0]; // 현재 활성화된(visible 클래스가 붙은) .graphic-item을 지정
+  // 현재 활성화된(visible 클래스가 붙은) .graphic-item을 지정
+  let currentItem = graphicElems[0];
+  let ioIndex;
+
+  const io = new IntersectionObserver((entries, observer) => {
+    ioIndex = Number(entries[0].target.dataset.index);
+  });
 
   // data-index 속성 초기화
   for (let i = 0; i < stepElems.length; i++) {
+    io.observe(stepElems[i]);
     stepElems[i].dataset.index = i;
     graphicElems[i].dataset.index = i;
   }
@@ -27,8 +34,14 @@
     let step;
     let boundingRect;
 
-    for (let i = 0; i < stepElems.length; i++) {
+    // for (let i = 0; i < stepElems.length; i++) {
+    for (let i = ioIndex - 1; i < ioIndex + 2; i++) {
       step = stepElems[i];
+
+      if (!step) {
+        continue;
+      }
+
       boundingRect = step.getBoundingClientRect();
 
       if (
@@ -36,11 +49,13 @@
         boundingRect.top < window.innerHeight * 0.8
       ) {
         inactivate();
-        currentItem = graphicElems[step.dataset.index]; // 스크롤 위치에 따라 현재 컨텐츠 변경
+        // 스크롤 위치에 따라 현재 컨텐츠 변경
+        currentItem = graphicElems[step.dataset.index];
         activate();
       }
     }
   });
 
-  activate(); // 첫 번째 컨텐츠 활성화
+  // 첫 번째 컨텐츠 활성화
+  activate();
 })();
